d892a8b434f9708cc59a5022d50ad66580ea3acc
Ramabhadra p. Rao <rpentako@BLR.ALLEGISINDIA.COM>
2018-01-31 15:09:26 +0530

    initial commit php working.

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php 
+define("CLIENT_ID", "3MVG9d8..z.hDcPIne5.lZ6bSlIPA1t5tlkKFKEY4.pEq6Zv4SZ5hTPsoLaBdx7FjQ.8LBvjusb6S7vajxqGe"); 
+define("CLIENT_SECRET", "6862960476408714468"); 
+define("REDIRECT_URI", "https://localhost/resttest/oauth_callback.php"); 
+define("LOGIN_URI", "https://login.salesforce.com"); 
+?> 
